189065c2fc9472fb0c7d82f0cb82701f8c245c82
Tomoki Hayashi <hayashi.tomoki@outlook.com>
2020-12-08 12:50:06 +0900

    [wip]EnventrySystem

---
+++ Unity/UniMoonDialogue/Assets/UniMoonDialogue/Scripts/EnventrySystem/EnventryEngine.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Events;
+
+namespace UniMoonDialogue.Enventry
+{
+    public class EnventryEngine : SingletonMonoBehaviour<EnventryEngine>
+    {
+        public UnityAction<ItemType, EnventryItemBase> OnMyEnventryUpdated;
+
+        public EnventryEngine()
+        {
+            BuildDataBase();
+        }
+
+        private List<EnventryItemBase> items = new List<EnventryItemBase>();
+        private List<EnventryItemBase> myItems = new List<EnventryItemBase>();
+
+        void BuildDataBase()
+        {
+            items = new List<EnventryItemBase>
+            {
+                new Item( name:"Sord1",maxStore:1),
+                new Item( name:"Sord2",maxStore:2),
+                new Item( name:"Sord3",maxStore:3)
+            };
+        }
+
+        public enum ItemType { Item, Mission }
+        public enum ItemStoreResult { Success = 0, Notpermmit, NotFound, AlreadyMax }
+
+        public List<EnventryItemBase> GetAllItemList(ItemType type)
+        {
+            return items.Where(item => item.type == type).ToList();
+        }
+
+        public List<EnventryItemBase> GetMyItemList(ItemType type)
+        {
+            return myItems.Where(item => item.type == type).ToList();
+        }
+
+        public bool GetItem(string itemName, ItemType type, out ItemStoreResult result)
+        {
+            var item = items.First(x => x.name == itemName && x.type == type);
+            if (item != null)
+            {
+                var ownedItem = myItems.FirstOrDefault(_ => _.name == item.name && _.type == type);
+
+                //持ってなかったら新規で取得
+                if (ownedItem == null)
+                {
+                    item.currentStore = 1;
+                    myItems.Add(item);
+                    result = ItemStoreResult.Success;
+                    OnMyEnventryUpdated?.Invoke(type, item);
+                    return true;
+                }
+                //持ってたら追加
+                else if (ownedItem.currentStore < ownedItem.maxStore)
+                {
+                    ownedItem.currentStore++;
+                    result = ItemStoreResult.Success;
+                    OnMyEnventryUpdated?.Invoke(type, ownedItem);
+
+                    return true;
+                }
+                //もう持てなかったらエラー
+                else
+                {
+                    result = ItemStoreResult.AlreadyMax;
+                    return false;
+                }
+            }
+
+            result = ItemStoreResult.NotFound;
+            return false;
+        }
+
+        public bool UseItem(string itemName, ItemType type, out ItemStoreResult result)
+        {
+            var ownedItem = myItems.FirstOrDefault(_ => _.name == itemName && _.type == type);
+            //持ってない場合
+            if (ownedItem == null)
+            {
+                result = ItemStoreResult.NotFound;
+                return false;
+            }
+            else
+            {
+                ownedItem.currentStore--;
+                if (ownedItem.currentStore < 1)
+                    myItems.Remove(ownedItem);
+                result = ItemStoreResult.Success;
+
+                OnMyEnventryUpdated?.Invoke(type, ownedItem);
+                return true;
+            }
+        }
+    }
+}
---
+++ Unity/UniMoonDialogue/Assets/UniMoonDialogue/Scripts/EnventrySystem/EnventryTest.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace UniMoonDialogue.Enventry
+{
+    public class EnventryTest : MonoBehaviour
+    {
+        EnventryEngine enventry;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Test1();
+            Test2();
+            Test3();
+        }
+
+        void Test1()
+        {
+            enventry = EnventryEngine.Instance;
+
+            foreach (var item in enventry.GetAllItemList(EnventryEngine.ItemType.Item))
+            {
+                Debug.Log($"{item.name}/{item.type}");
+            }
+        }
+
+        void Test2()
+        {
+            EnventryEngine.ItemStoreResult result;
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[0].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[0].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[0].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[1].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[1].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.GetItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[1].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+
+            ShowMyIntentryItem();
+
+        }
+
+        void Test3()
+        {
+            EnventryEngine.ItemStoreResult result;
+            enventry.UseItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[0].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.UseItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[0].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+            enventry.UseItem(enventry.GetAllItemList(EnventryEngine.ItemType.Item)[1].name, EnventryEngine.ItemType.Item, out result);
+            Debug.Log(result);
+
+            ShowMyIntentryItem();
+        }
+
+        void ShowMyIntentryItem(EnventryEngine.ItemType type = EnventryEngine.ItemType.Item)
+        {
+            foreach (var item in enventry.GetMyItemList(type))
+            {
+                Debug.Log($"[{item.type}]{item.name} ({item.currentStore}/{item.maxStore})");
+            }
+        }
+
+    }
+}
